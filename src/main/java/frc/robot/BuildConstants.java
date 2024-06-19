b1ff054faa7046989b52ff458603d5521368f3a8
barbute <jackhdemeterio@gmail.com>
2024-06-18 19:05:26 -0500

    Wrote swerve module simulation code

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Forte";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 4;
-  public static final String GIT_SHA = "45dda34c2bb71937244bb9a3ee8b1835d2a65b1a";
-  public static final String GIT_DATE = "2024-06-18 18:33:32 CDT";
+  public static final int GIT_REVISION = 5;
+  public static final String GIT_SHA = "487828d1ae945eff549291efcaf132087c7510b8";
+  public static final String GIT_DATE = "2024-06-18 18:59:29 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-06-18 18:59:07 CDT";
-  public static final long BUILD_UNIX_TIME = 1718755147389L;
+  public static final String BUILD_DATE = "2024-06-18 19:04:58 CDT";
+  public static final long BUILD_UNIX_TIME = 1718755498948L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/drive/ModuleIOSim.java
+++ src/main/java/frc/robot/drive/ModuleIOSim.java
@@ -4,15 +4,53 @@
 
 package frc.robot.drive;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
 /** Physics sim implementation of ModuleIO */
 public class ModuleIOSim implements ModuleIO {
+  private final double LOOP_PERIOD_S = 0.02;
+  // TODO Set these for Forte's gear ratios
+  private static final double DRIVE_GEAR_RATIO = 1.0 / 1.0;
+  private static final double AZIMUTH_GEAR_RATIO = 1.0 / 1.0;
+
+  private DCMotorSim driveMotor = new DCMotorSim(DCMotor.getNEO(1), DRIVE_GEAR_RATIO, 0.025);
+  private DCMotorSim azimuthMotor = new DCMotorSim(DCMotor.getNEO(1), AZIMUTH_GEAR_RATIO, 0.004);
+
+  private final Rotation2d azimuthAbsoluteInitPosition =
+      new Rotation2d(Math.random() * 2.0 * Math.PI);
+
+  private double driveAppliedVolts = 0.0;
+  private double azimuthAppliedVolts = 0.0;
 
   @Override
-  public void updateInputs(ModuleIOInputs inputs) {}
+  public void updateInputs(ModuleIOInputs inputs) {
+    inputs.drivePositionRad = driveMotor.getAngularPositionRad();
+    inputs.driveVelocityRadPerSec = driveMotor.getAngularVelocityRadPerSec();
+    inputs.driveAppliedVolts = driveAppliedVolts;
+    inputs.driveCurrentAmps = new double[] {Math.abs(driveMotor.getCurrentDrawAmps())};
+    inputs.driveTemperatureCelsius = new double[] {0.0};
+
+    inputs.azimuthAbsolutePosition =
+        new Rotation2d(azimuthMotor.getAngularPositionRad()).plus(azimuthAbsoluteInitPosition);
+    inputs.azimuthPosition = Rotation2d.fromRadians(azimuthMotor.getAngularPositionRad());
+    inputs.azimuthVelocityRadPerSec = azimuthMotor.getAngularVelocityRadPerSec();
+    inputs.azimuthAppliedVolts = azimuthAppliedVolts;
+    inputs.azimuthCurrentAmps = new double[] {Math.abs(azimuthMotor.getCurrentDrawAmps())};
+    inputs.azimuthTemperatureCelsius = new double[] {0.0};
+  }
 
   @Override
-  public void setDriveVolts(double volts) {}
+  public void setDriveVolts(double volts) {
+    driveAppliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
+    driveMotor.setInputVoltage(driveAppliedVolts);
+  }
 
   @Override
-  public void setAzimuthVolts(double volts) {}
+  public void setAzimuthVolts(double volts) {
+    azimuthAppliedVolts = MathUtil.clamp(volts, -12.0, 12.0);
+    azimuthMotor.setInputVoltage(azimuthAppliedVolts);
+  }
 }
