16b6c6cb1b035e62858bbb2ddc39d8bd15a95f16
barbute <jackhdemeterio@gmail.com>
2024-06-20 17:20:30 -0500

    Instantiated Drive in RobotContainer

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "Forte";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 11;
-  public static final String GIT_SHA = "8956666f328887841176013bc7b5982972ab6509";
-  public static final String GIT_DATE = "2024-06-18 19:42:10 CDT";
+  public static final int GIT_REVISION = 12;
+  public static final String GIT_SHA = "c0c25b01f11c0a9955561249199b84f9f839db89";
+  public static final String GIT_DATE = "2024-06-18 19:47:27 CDT";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2024-06-18 19:45:52 CDT";
-  public static final long BUILD_UNIX_TIME = 1718757952814L;
+  public static final String BUILD_DATE = "2024-06-20 17:19:26 CDT";
+  public static final long BUILD_UNIX_TIME = 1718921966078L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,62 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.DriveCommands;
+import frc.robot.subsystems.drive.Drive;
+import frc.robot.subsystems.drive.GyroIO;
+import frc.robot.subsystems.drive.GyroIOPigeon2;
+import frc.robot.subsystems.drive.ModuleIO;
+import frc.robot.subsystems.drive.ModuleIOSim;
+import frc.robot.subsystems.drive.ModuleIOSparkMax;
 
 public class RobotContainer {
+  private Drive robotDrive;
+
+  private CommandXboxController pilotController = new CommandXboxController(0);
+
   public RobotContainer() {
+    switch (Constants.currentMode) {
+      case REAL:
+        robotDrive =
+            new Drive(
+                new GyroIOPigeon2(),
+                new ModuleIOSparkMax(0),
+                new ModuleIOSparkMax(1),
+                new ModuleIOSparkMax(2),
+                new ModuleIOSparkMax(3));
+        break;
+      case SIM:
+        robotDrive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim(),
+                new ModuleIOSim());
+        break;
+      default:
+        robotDrive =
+            new Drive(
+                new GyroIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {},
+                new ModuleIO() {});
+        break;
+    }
+
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    robotDrive.setDefaultCommand(
+        DriveCommands.joystickDrive(
+            robotDrive,
+            () -> pilotController.getLeftX(),
+            () -> pilotController.getLeftY(),
+            () -> pilotController.getRightX()));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
